5c6da95bee3670ee8a99b1ba1d329d7f8b69fc3c
Suthee Gomootsukhavadee <101073146+SutheeDev@users.noreply.github.com>
2026-06-28 19:59:53 -0400

    Remove dead code

--- Backend/server.mjs
+++ Backend/server.mjs
@@ -7,9 +7,6 @@ import morgan from "morgan";
 import connectDB from "./config/db.mjs";
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.mjs";
 
-// test
-import { body, validationResult } from "express-validator";
-
 dotenv.config();
 const app = express();
 
@@ -38,24 +35,6 @@ app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/restaurants", restaurantRouter);
 
-// Express-Validation Test Route
-app.post(
-  "/api/v1/test",
-  [body("name").notEmpty().withMessage("Name is required")],
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      const errorMessages = errors.array().map((error) => error.msg);
-      return res.status(400).json({ errors: errorMessages });
-    }
-    next();
-  },
-  (req, res) => {
-    const { name } = req.body;
-    res.json({ message: `hello ${name}` });
-  }
-);
-
 // Not Found Middleware
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "Not Found" });
@@ -63,9 +42,6 @@ app.use("*", (req, res) => {
 
 // Error Middleware
 app.use(errorHandlerMiddleware);
-// app.use((err, req, res, next) => {
-//   res.status(500).json({ msg: "Something went wrong" });
-// });
 
 const start = async () => {
   try {
